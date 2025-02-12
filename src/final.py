4286b0e93465705504ab47c7b1bf5066fd2239ab
nabin2004 <olinabin2004@gmail.com>
2025-02-12 16:59:24 +0545

    Fixed the error with new_requirements.txt

--- src/final.py
+++ src/final.py
@@ -1,7 +1,5 @@
-import nemo
-import nemo.collections.asr as asr
-from omegaconf import OmegaConf, open_dict
 import nemo.collections.asr as nemo_asr
+from omegaconf import OmegaConf
 import pytorch_lightning as pl
 
 params = OmegaConf.load("./contextnet_rnnt.yaml")
@@ -10,33 +8,10 @@ print(OmegaConf.to_yaml(params))
 
 params.model.tokenizer.dir = "../tokenizer/"
 params.model.tokenizer.type = "bpe"
-
-
-trainer1 = pl.Trainer(devices=1, max_epochs=10)
-
-first_asr_model = nemo_asr.models.EncDecCTCModelBPE(cfg=params.model, trainer=trainer1)
-
-trainer1.fit(first_asr_model)
-
-import nemo
-import nemo.collections.asr as asr
-from omegaconf import OmegaConf, open_dict
-import nemo.collections.asr as nemo_asr
-import pytorch_lightning as pl
-
-params = OmegaConf.load("./contextnet_rnnt.yaml")
-
-print(OmegaConf.to_yaml(params))
-
-params.model.tokenizer.dir = "../tokenizer/"
-params.model.tokenizer.type = "bpe"
-
 
 trainer1 = pl.Trainer(devices=1, max_epochs=10)
 
 asr_model = nemo_asr.models.EncDecCTCModelBPE(cfg=params.model, trainer=trainer1)
-
-# nemo_asr.models.EncDecRNNTModel
 
 trainer1.fit(asr_model)
 
